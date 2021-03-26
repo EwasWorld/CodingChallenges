246fedd73c274d440696f084f40f963b203bc601
Ewa <ella.caldeirahankey@ntlworld.com>
2021-03-26 18:42:13 +0000

    GoogleCodeJam2021: Problem 1 submitted

--- src/googleCodeJam/mar2021/qualifying/problem1/Problem1.kt
+++ src/googleCodeJam/mar2021/qualifying/problem1/Problem1.kt
@@ -1,10 +1,61 @@
 package googleCodeJam.mar2021.qualifying.problem1
 
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
 import java.util.*
 
-val input = Scanner(BufferedReader(InputStreamReader(System.`in`)))
-fun main() {
 
+/*
+Musings:
+
+4213
+4[213]
+position 3
+reverse 1-3
+1243
+*/
+
+val input = Scanner(BufferedReader(InputStreamReader(System.`in`)))
+//val input = Scanner(File("src\\googleCodeJam\\mar2021\\qualifying\\problem1\\testInput.txt"))
+
+fun main() {
+    val totalCases = Integer.parseInt(input.nextLine())
+    for (caseNumber in 1..totalCases) {
+        val listSize = Integer.parseInt(input.nextLine())
+        var list = input.nextLine().split(" ").map { Integer.parseInt(it) }
+        check(list.size == listSize) { "UnexpectedSize" }
+
+        var cost = 0
+        // Skip the last iteration, list will be sorted
+        for (i in 0 until list.size - 1) {
+            /*
+             * Locate minimum between i and end of list inclusive
+             */
+            var minPos = i
+            var minVal = list[i]
+            for (j in (i + 1) until list.size) {
+                if (list[j] < minVal) {
+                    minPos = j
+                    minVal = list[j]
+                }
+            }
+
+            /*
+             * Reverse the sublist from i to minPos inclusive
+             */
+            if (minPos != i) {
+                list = list.subList(0, i)
+                        .plus(list.subList(i, minPos + 1).reversed())
+                        .plus(list.subList(minPos + 1, list.size))
+            }
+
+            /*
+             * Calculate cost
+             */
+            // Cost is size of list that was reversed
+            cost += minPos - i + 1 // +1 because inclusive, and yes, they count reversing a singleton as cost 1...
+        }
+        println("Case #$caseNumber: $cost")
+    }
 }
---
+++ src/googleCodeJam/mar2021/qualifying/problem2/Problem2.kt
@@ -0,0 +1,200 @@
+package googleCodeJam.mar2021.qualifying.problem2
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.IllegalArgumentException
+import java.util.*
+import kotlin.math.abs
+
+enum class ArtElement { C, J, X;
+    fun getOpposite(): ArtElement {
+        return when (this) {
+            C -> J
+            J -> C
+            else -> throw IllegalArgumentException("no opposite defined");
+        }
+    }
+}
+
+val input = Scanner(BufferedReader(InputStreamReader(System.`in`)))
+
+fun main() {
+    val totalCases = Integer.parseInt(input.nextLine())
+    for (caseNumber in 1..totalCases) {
+        val line = input.nextLine().split(" ")
+        val cjCost = Integer.parseInt(line[0])
+        val jcCost = Integer.parseInt(line[1])
+        var art = mutableListOf<ArtElement>()
+        for (element in line[3]) {
+            try {
+                art.add(ArtElement.valueOf(element.toUpperCase().toString()))
+            }
+            catch (e: IllegalArgumentException) {
+                art.add(ArtElement.X)
+            }
+        }
+
+        /*
+         * Fill start and end unknowns
+         */
+        if (art[0] != ArtElement.X) {
+            art = fillUnknownsAtStart(art, cjCost, jcCost)
+        }
+        if (art.last() != ArtElement.X) {
+            // Pretend the end is the start :P
+            art = fillUnknownsAtStart(art.asReversed(), jcCost, cjCost).asReversed()
+        }
+
+        /*
+         * Fill unknowns that fall between two known elements
+         */
+        var firstUnknownIndex: Int? = null
+        for (i in art.indices) {
+            // Find start of unknown sequence
+            if (art[i] == ArtElement.X && firstUnknownIndex == null) {
+                firstUnknownIndex = i
+                continue
+            }
+            // Check if end of unknown sequence
+            if (firstUnknownIndex != null) {
+                var startElement = art[firstUnknownIndex - 1]
+                val endElement = art[i + 1]
+
+                // Convert to a sequence that starts and ends on the same element by filling the first unknown
+                //      (there's no way to avoid making at least one pair of this type anyway)
+                if (startElement != endElement) {
+                    art[firstUnknownIndex] = endElement
+                    startElement = endElement
+                    firstUnknownIndex++
+                }
+                if (firstUnknownIndex <= i) {
+                    val filling = generateElements(startElement, i - firstUnknownIndex + 1, cjCost + jcCost < 0).asReversed()
+                    // Replace the slice of unknowns in the artwork with the new filling
+                    art.filterIndexed { index, _ -> index <= i && index >= firstUnknownIndex ?: index + 1 }
+                    art.addAll(firstUnknownIndex, filling)
+                }
+                firstUnknownIndex = null
+            }
+        }
+
+        println("Case #$caseNumber: " + calculateArtCost(art, cjCost, jcCost))
+    }
+}
+
+/**
+ * @return minimum cost list if all elements are unknowns
+ */
+fun fillListOfUnknowns(size: Int, cjCost: Int, jcCost: Int): MutableList<ArtElement> {
+    // Both positive or trivially sized list: cost 0
+    if (cjCost >= 0 && jcCost >= 0 || size == 1) {
+        return generateElements(ArtElement.J, size, false)
+    }
+    // At least one negative but cost of alternating would be high: just do a single negative-cost pair
+    if (cjCost + jcCost > 0 && size > 2) {
+        val startElement = if (cjCost < 0) ArtElement.C else ArtElement.J
+        val art = generateElements(startElement.getOpposite(), size, false)
+        art[0] = startElement
+        return art
+    }
+    // Alternate starting on the negative(est)-cost pair to ensure it maximises that one
+    return generateElements(if (cjCost < jcCost) ArtElement.C else ArtElement.J, size, true)
+}
+
+/**
+ * @param firstKnownElement in an alternating list, it assumes it's generating unknown items AFTER this, therefore the
+ * first item will be opposite. In a non-alternating list, this item will be duplicated for the whole list
+ * @param alternate true: alternates between J and C starting with the opposite of [firstKnownElement]. False: generates
+ * a list of [firstKnownElement]
+ */
+fun generateElements(firstKnownElement: ArtElement, count: Int, alternate: Boolean): MutableList<ArtElement> {
+    return MutableList(count) { index -> if (alternate && index % 2 == 0) firstKnownElement.getOpposite() else firstKnownElement }
+}
+
+fun calculateArtCost(art: List<ArtElement>, cjCost: Int, jcCost: Int): Int {
+    var cost = 0
+    var previousElement: ArtElement? = null
+    for (element in art) {
+        if (previousElement != null && previousElement != element) {
+            cost += if (previousElement == ArtElement.C) cjCost else jcCost
+        }
+        previousElement = if (element != ArtElement.X) element else null
+    }
+    return cost
+}
+
+/**
+ * If [art] starts with N ArtElement.X elements, it will optimise the first N elements and return an ammended art piece
+ */
+fun fillUnknownsAtStart(art: MutableList<ArtElement>, cjCost: Int, jcCost: Int): MutableList<ArtElement> {
+    var numberOfPlacesToFill = art.indexOfFirst { it != ArtElement.X } - 1
+    if (numberOfPlacesToFill == -1) {
+        return fillListOfUnknowns(art.size, cjCost, jcCost)
+    }
+    val firstKnownItem = art[numberOfPlacesToFill + 1]
+    val costSignIdentical = (cjCost <= 0 && jcCost <= 0) || (cjCost >= 0 && jcCost >= 0)
+    val cjIsNegativeCost = cjCost < 0
+
+    var forceNegativeEnding = false
+    val alternate: Boolean
+    if (costSignIdentical && !cjIsNegativeCost) {
+        alternate = false
+    }
+    else if (numberOfPlacesToFill == 1) {
+        if (costSignIdentical && cjIsNegativeCost) {
+            alternate = true
+        }
+        else {
+            alternate = false
+            if (firstKnownItem == ArtElement.J && cjIsNegativeCost) {
+                art[--numberOfPlacesToFill] = ArtElement.C
+            }
+            if (firstKnownItem == ArtElement.C && !cjIsNegativeCost) {
+                art[--numberOfPlacesToFill] = ArtElement.J
+            }
+        }
+    }
+    else if (costSignIdentical && cjIsNegativeCost) {
+        alternate = true
+    }
+    else if ((cjIsNegativeCost && abs(cjCost) >= jcCost) || (!cjIsNegativeCost && abs(jcCost) >= cjCost)) {
+        alternate = true
+        forceNegativeEnding = true
+    }
+    else {
+        alternate = false
+        if (firstKnownItem == ArtElement.J && cjIsNegativeCost) {
+            art[--numberOfPlacesToFill] = ArtElement.C
+        }
+        if (firstKnownItem == ArtElement.C && !cjIsNegativeCost) {
+            art[--numberOfPlacesToFill] = ArtElement.J
+        }
+    }
+
+    val finalArt = generateElements(firstKnownItem, numberOfPlacesToFill, alternate).plus(art.subList(numberOfPlacesToFill + 1, art.size - 1)).toMutableList()
+    if (forceNegativeEnding && art[0] == ArtElement.C && !cjIsNegativeCost) {
+        finalArt[0] = ArtElement.J
+    }
+    return finalArt
+}
+
+/* Musings:
+If both costs are positive
+    Duplicate the last item
+Else if there's just one element to assign
+    If both costs are negative
+        Alternate
+    Else
+        Make the negative-cost pair or no pair
+Else if both costs are negative
+    Alternate
+Else if abs(negative cost) >= positive cost
+    Alternate making sure to end on a negative
+Else
+    Make a single negative-cost pair or no pair
+
+Actions:
+No pair
+Alternate
+Alternate and end on best negative
+Force negative or no pair
+*/
